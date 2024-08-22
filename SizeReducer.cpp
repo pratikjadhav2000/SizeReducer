5d643b86a7799195251feca6ccf830da8e0d5e4f
Pratik Jadhav <pratikjadhav.52239@gmail.com>
2024-08-23 02:29:19 +0530

    version 1.0

---
+++ SizeReducer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    string fileName;
+
+    // list .mp4 files in list.txt
+    system("(for %a in (*.mp4) do @echo %a) > list.txt");
+
+    // open file list.txt to read .mp4 files
+    ifstream fin("list.txt");
+
+    // Reduce size of files listed in list.txt
+    while (getline(fin, fileName))
+    {
+
+        string sizeReduceCommandString = "ffmpeg -i \"" + fileName + "\" -vcodec libx264 -crf 30 \"Reduced_" + fileName + "\"";
+        char *sizeReduceCommand = (char *)sizeReduceCommandString.data();
+        system(sizeReduceCommand);
+    }
+
+    fin.close();
+
+    cout << "\n \n Completed! Press any key to exit..." << endl;
+    getch();
+
+    // Delete the temporary file list.txt
+    system("del list.txt");
+
+    return 0;
+}
